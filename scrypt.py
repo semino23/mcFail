94dd77e647f36e3a6d527f16e5aacc2cc75e6a7b
Semino <seminokoinzer406@gmail.com>
2025-07-20 22:58:49 +0200

    adding sql queryies

--- scrypt.py
+++ scrypt.py
@@ -3,7 +3,9 @@ import random
 from ipaddress import IPv4Network
 import mcstatus
 import sqlite3
+from mcstatus.responses import JavaStatusPlayers
 
+DATABASE = "test.sqlite"
 
 class IpGenerator:
     excluded_networks: list[ipaddress.IPv4Network] = [
@@ -55,34 +57,74 @@ class IpGenerator:
 
 
 def prepare_database():
-    cur = sqlite3.connect(":memory:").cursor()
+    cur = sqlite3.connect(DATABASE).cursor()
     cur.execute("""
-   create table `players` (
+   create table IF NOT EXISTS `players` (
   `uuid` TEXT not null,
   `name` TEXT not null,
   primary key (`uuid`)
 );""")
 
-    cur.execute("""create table `server` (
-  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
-  `name` TEXT not null,
-  `slots` INTEGER null,
-  `version` TEXT null);""")
+    cur.execute("""create table IF NOT EXISTS `servers` (
+  `address` TEXT not null,
+  `slots` INTEGER not null,
+  `online` INTEGER not null,
+  `version` TEXT not null,
+  `description` TEXT not null,
+  `motd` TEXT not null,
+   primary key (`address`)  
+  );""")
 
     cur.execute("""
-create table `player_log` (
+create table IF NOT EXISTS`player_log` (
   `player_id` TEXT not null,
   `server_id` INTEGER not null,
-  `column_3` DATETIME not null default current_time
+  `time` DATETIME not null default current_time
 );""")
 
 
-def log_occurance(server: mcstatus.responses.JavaStatusResponse):
-    print(f"found server:\t{server.players}")
-    if server.players.sample:
-        print(f"with players:\t{server.players.sample}")
-    else:
-        print("with no Player")
+def log_occurance(servers: mcstatus.server.JavaServer):
+
+
+    def insert_player_if_not_exists(log_data:JavaStatusPlayers , db_conn:sqlite3.Connection):
+        player_data = [(player.name , player.uuid) for player in log_data.sample]
+        print(player_data)
+        db_conn.cursor().executemany("INSERT INTO players (name , uuid) VALUES (? , ?)", player_data)
+        db_conn.commit()
+
+
+    def insert_server_if_not_exists(log_data:mcstatus.server.JavaServer , db_conn:sqlite3.Connection):
+        server_status = log_data.status()
+
+        server_data = (
+            server_status.players.max,
+            server_status.players.online,
+            server_status.version,
+            log_data.address,
+            server_status.motd.parsed,
+            server_status.description
+        )
+
+        print(server_data)
+        db_conn.cursor().execute("INSERT INTO servers (slots , online , version , address , motd , description) VALUES ( ? , ?, ? , ? , ? , ?)" , server_data)
+        db_conn.commit()
+
+
+    def log(log_data: mcstatus.JavaServer ,db_conn:sqlite3.Connection):
+        insert_player_if_not_exists(log_data.status().players, db_conn)
+
+        session_log_data = [(player.uuid , log_data.address) for player in log_data.status().players.sample]
+        db_conn.cursor().executemany("INSERT INTO player_log (player_id , server_id) (?,?)" , session_log_data)
+        db_conn.commit()
+
+
+    data = servers.status()
+    con = sqlite3.connect(DATABASE)
+
+
+    insert_server_if_not_exists(servers, con)
+    if data.players.sample:
+        log(servers , con)
 
 
 def scan(ip_range: IPv4Network):
@@ -91,7 +133,7 @@ def scan(ip_range: IPv4Network):
     for target in ips:
         try:
             res: mcstatus.JavaServer = mcstatus.JavaServer.lookup(str(target), 5)
-            log_occurance(res.status())
+            log_occurance(res)
         except:
             pass
             # print(e)
