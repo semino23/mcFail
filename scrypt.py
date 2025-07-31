da88af7a8e913b1be371566281d8cdb647ac956d
anarchaos <seminokoinzer406@gmail.com>
2025-07-31 16:11:03 +0200

    scanning the internet every 20 minutes

--- scrypt.py
+++ scrypt.py
@@ -1,13 +1,11 @@
 from ipaddress import IPv4Network
-
+from time import sleep
 from src.scanner import scan, IpGenerator
 
 ips = IpGenerator.generate_ips()
 
-#while False:
-    # continuous scan
+while True:
+    for ipRange in ips:
+        scan(ipRange)
+    sleep(20 * 60)
 
-#for i in ips:
-#    scan(i)
-
-scan(IPv4Network("127.0.0.1/32"))
