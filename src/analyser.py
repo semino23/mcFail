f37c928f36977e5d2ce026a18d1ab097a5767787
anarchaos <seminokoinzer406@gmail.com>
2025-07-31 15:48:57 +0200

    seperating analysation to secondary project

--- src/analyser.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-DATABASE = "test.sqlite"
-
-
-def collect_player():
-    cur = sqlite3.connect(DATABASE).cursor()
-    players = cur.execute("SELECT * FROM players").fetchmany()
-    print(players)
